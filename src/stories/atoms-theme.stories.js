0b771d0dca83bce5f41d400b06bffb961c95f062
mion/Mion <m.higaki@4nonome.com>
2022-09-20 17:10:17 +0900

    atoms-theme関連ファイル3つ追加

---
+++ src/stories/atoms-theme.stories.js
@@ -0,0 +1,6 @@
+import Item_Theme from "../components/atoms/theme/theme.html";
+import "../components/atoms/theme/theme.scss";
+
+export default { title: "Atoms/Theme" };
+
+export const Theme = () => Item_Theme;
