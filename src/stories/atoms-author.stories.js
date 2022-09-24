151d533b9886aa35b008de16efe1fc6c1e9c3502
mion/Mion <m.higaki@4nonome.com>
2022-09-24 21:50:31 +0900

    atoms/authorを追加

---
+++ src/stories/atoms-author.stories.js
@@ -0,0 +1,6 @@
+import Item_Author from "../components/atoms/author/author.html";
+import "../components/atoms/author/author.scss";
+
+export default { title: "Atoms/Author" };
+
+export const Theme = () => Item_Author;
