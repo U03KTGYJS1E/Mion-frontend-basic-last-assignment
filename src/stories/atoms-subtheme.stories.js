de272bb92aa310604127e59272a83d6dc3fcf944
mion/Mion <m.higaki@4nonome.com>
2022-09-24 21:44:50 +0900

    atoms/subthemeを追加

---
+++ src/stories/atoms-subtheme.stories.js
@@ -0,0 +1,6 @@
+import Item_Theme from "../components/atoms/subtheme/subtheme.html";
+import "../components/atoms/subtheme/subtheme.scss";
+
+export default { title: "Atoms/Subyarn Theme" };
+
+export const Theme = () => Item_Theme;
