6faaa96afc86b71a71fce1e0b873b0670c293152
mion/Mion <m.higaki@4nonome.com>
2022-09-25 10:06:38 +0900

    atoms/buttonを追加

---
+++ src/stories/atoms-button.stories.js
@@ -0,0 +1,6 @@
+import Item_Button from "../components/atoms/name/name.html";
+import "../components/atoms/button/button.scss";
+
+export default { title: "Atoms/Button" };
+
+export const Theme = () => Item_Button;
